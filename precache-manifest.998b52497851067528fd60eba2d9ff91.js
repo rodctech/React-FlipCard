54ddeba88f54f15681d81f4c04ce4408b6b4596c
Rodrigo Chavez <rodctech@gmail.com>
2019-06-27 11:36:32 -0500

    Updates

---
+++ precache-manifest.998b52497851067528fd60eba2d9ff91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa16bc38ea4d3362d9e0a01e931e1116",
+    "url": "/React-FlipCard/index.html"
+  },
+  {
+    "revision": "d15a2c6b2a0b24ead813",
+    "url": "/React-FlipCard/static/css/main.16c95252.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/React-FlipCard/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "d15a2c6b2a0b24ead813",
+    "url": "/React-FlipCard/static/js/main.e6421373.chunk.js"
+  },
+  {
+    "revision": "cf670ae6e5a8785422c1",
+    "url": "/React-FlipCard/static/js/runtime~main.dbce3032.js"
+  }
+]);
